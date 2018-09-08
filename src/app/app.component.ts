88e0566836d2ca4fb044c9197b8eaf81cc147705
Lucas Cunha <you@example.com>
2018-09-08 13:35:50 -0300

    Projeto criado, criado componentes header e home

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from "@angular/core"
+
+@Component({
+  selector: 'if-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
