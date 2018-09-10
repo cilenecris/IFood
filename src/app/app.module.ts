5978364c1ad3d81fb3c8fff37f26fc6ade64abc6
Lucas Cunha <you@example.com>
2018-09-10 01:12:20 -0300

    Criado um componente para o input, validação do formulário melhorada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpModule } from "@angular/http";
+import { NgModule, LOCALE_ID } from "@angular/core";
+
 import { ROUTES } from "./app.routes";
 
+import { HttpModule } from "@angular/http";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -12,13 +13,16 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
-
-import { RestaurantsService } from "./restaurants/restaurants.service";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { OrderComponent } from "./order/order.component";
+
+import { RestaurantsService } from "./restaurants/restaurants.service";
+import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
+import { InputComponent } from './shared/input/input.component';
 
 @NgModule({
   declarations: [
@@ -32,10 +36,21 @@ import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component"
     MenuComponent,
     ShoppingCartComponent,
     MenuItemComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    OrderComponent,
+    InputComponent
   ],
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
-  providers: [RestaurantsService, ShoppingCartService],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [
+    RestaurantsService,
+    ShoppingCartService,
+    { provide: LOCALE_ID, useValue: "pt-BR" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/order/order.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'if-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
